29d0a4ad4a7acacf58ab831d0dc80ef47c3b3adb
Pranjal Kumar Maurya <workwithpranjal@gmail.com>
2023-03-23 00:02:44 +0530

    Home page done

--- main.js
+++ main.js
@@ -4,3 +4,20 @@ window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle
     ('window-scroll', window.scrollY > 250)
 })
+
+//show hidden faq answer
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        //change plus icon
+        const icon = faq.querySelector('.faq_icon i');
+        if(icon.className === 'uil uil-plus') {
+            icon.className = 'uil uil-angle-down';
+        }
+        else icon.className = 'uil uil-plus';     
+    })
+})
